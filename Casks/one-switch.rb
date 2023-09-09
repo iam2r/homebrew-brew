e3ad2e3e4a142f19121c18e3ad786d81e10efb9b
iamrazo <bestwishtous@gmail.com>
2023-09-09 23:01:40 +0800

    feature: 添加国内可访问代理

--- Casks/one-switch.rb
+++ Casks/one-switch.rb
@@ -2,7 +2,7 @@ cask "one-switch" do
     version "1.31"
     sha256 :no_check
 
-    url "https://drive.usercontent.google.com/download?id=1lp6wDfrMrYU-jSCYgFtwMpMOStUCya3Y&export=download&authuser=0&confirm=t&uuid=40381826-9f02-49d8-8bf7-cf4e4a4ab55f&at=APZUnTXh4hVEUbaiNiBuVQKsuC37:1693740286934"
+    url "https://drive.usercontent.google.com.proxy.iamrazo.pro/download?id=1lp6wDfrMrYU-jSCYgFtwMpMOStUCya3Y&export=download&authuser=0&confirm=t&uuid=40381826-9f02-49d8-8bf7-cf4e4a4ab55f&at=APZUnTXh4hVEUbaiNiBuVQKsuC37:1693740286934"
     name "One Switch"
     desc "All system and utility switches in one place"
     homepage "https://fireball.studio/oneswitch"
