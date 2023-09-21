432dc25af80ac495faac6591ecb3be0a2932aff7
iamrazo <bestwishtous@gmail.com>
2023-09-21 12:57:04 +0800

    feature: update clash-verge

--- Casks/clash-verge.rb
+++ Casks/clash-verge.rb
@@ -1,9 +1,8 @@
 cask "clash-verge" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.3.6"
-  sha256 arm:   "6b3950f7b1251ee22e66fc28fd1cde4e3024910fd7ccf743ba1c715ebff1b81e",
-         intel: "66db87ec09761e49eab542369cae16579f1552d17a19483502bfacaac326df97"
+  version "1.3.7"
+  sha256 :no_check
 
   url "https://github.com/zzzgydi/clash-verge/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
   name "Clash for Windows"
