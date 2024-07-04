6022d5484586009a5b0fea2b65cf9cf583c6c9f9
iamrazo <fake@fake.com>
2024-07-04 12:55:29 +0800

    feat: add paragon-camptune

---
+++ Casks/paragon-camptune.rb
@@ -0,0 +1,28 @@
+cask "paragon-camptune" do
+    version "10.13.433"
+    sha256 :no_check
+  
+    url "https://proxy.cf.181918.xyz/drive.usercontent.google.com/download?id=1A2WGwFRhGQR3OMcM5Pg6unw5Xg6OV1PJ&export=download&authuser=0&confirm=t&uuid=94868996-e444-4bf4-a24e-7af97a44a0e4&at=APZUnTUuqay_OXq_g7p0lXaVR-_b:1720068819101"
+    name "Paragon CampTune"
+    desc "Manage disk space on Macs with Boot Camp"
+    homepage "https://www.paragon-software.com/home/camptune/"
+    end
+  
+    depends_on macos: ">= :sierra"
+  
+    app "Paragon CampTune.app"
+  
+    uninstall delete: [
+                "/Library/Application Support/Paragon Software/com.paragon-software.camptunex.helper",
+                "/Library/Application Support/Paragon Software/com.paragon-software.camptunex.helper-data",
+                "/Library/Application Support/Paragon Software/uc.camptunex",
+              ],
+              rmdir:  "/Library/Application Support/Paragon Software/"
+  
+    zap trash: [
+      "~/Library/Application Support/com.paragon-software.camptunex",
+      "~/Library/HTTPStorages/com.paragon-software.camptunex",
+      "~/Library/Preferences/com.paragon-software.camptunex.plist",
+      "~/Library/Saved Application State/com.paragon-software.camptunex.savedState",
+    ]
+  end
