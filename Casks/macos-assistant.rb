84a62508d989997aff9694afe55739c4af7996e2
iamrazo <fake@fake.com>
2024-07-04 11:24:24 +0800

    fix: replace proxy host

--- Casks/macos-assistant.rb
+++ Casks/macos-assistant.rb
@@ -4,7 +4,7 @@ cask "macos-assistant" do
   version "1.3.3"
   sha256 :no_check
 
-  url "https://proxy.cf.iamrazo.pro/drive.usercontent.google.com/download?id=1N6qiU-OaC2PdbHuF6DZnOTThZ4rJXrGZ&export=download&confirm=t"
+  url "https://proxy.cf.181918.xyz/drive.usercontent.google.com/download?id=1N6qiU-OaC2PdbHuF6DZnOTThZ4rJXrGZ&export=download&confirm=t"
   name "MacOS Assistant"
   desc "MacOS Assistant is a script tool developed by macwk.com, which contains some commonly used macOS command line script codes, which can help you perform some operations quickly."
 
--- Casks/one-switch.rb
+++ Casks/one-switch.rb
@@ -2,7 +2,7 @@ cask "one-switch" do
     version "1.31"
     sha256 :no_check
 
-    url "https://proxy.cf.iamrazo.pro/drive.usercontent.google.com/download?id=1lp6wDfrMrYU-jSCYgFtwMpMOStUCya3Y&export=download&confirm=t"
+    url "https://proxy.cf.181918.xyz/drive.usercontent.google.com/download?id=1lp6wDfrMrYU-jSCYgFtwMpMOStUCya3Y&export=download&confirm=t"
     name "One Switch"
     desc "All system and utility switches in one place"
     homepage "https://fireball.studio/oneswitch"
